a9e9ed52df80fd10b19c7e81ed1f4614877bc421
abe88j <yuutajb3@gmail.com>
2015-12-03 22:58:22 +0900

    daily work finish

--- wolio/Assets/src/Piyo.cs
+++ wolio/Assets/src/Piyo.cs
@@ -5,11 +5,20 @@ public class Piyo : MonoBehaviour
 {
     [SerializeField]
     private int m_hp;
+    [SerializeField]
+    private float m_JumpForce;
+    [SerializeField]
+    private LayerMask m_WhatIsGround;
+    [SerializeField]
+    private float m_MaxSpeed = 10f;
+
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_isRunning;
 
     // Use this for initialization
     void Start()
     {
-
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,6 +28,9 @@ public class Piyo : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        Run();
+        Jump();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -26,6 +38,43 @@ public class Piyo : MonoBehaviour
         if (other.gameObject.tag == "Bullet")
         {
             m_hp--;
+        }
+    }
+
+    public void Run()
+    {
+        if (!m_isRunning)
+        {
+            StartCoroutine(RunCoroutine());
+        }
+    }
+
+    public IEnumerator RunCoroutine()
+    {
+        m_isRunning = true;
+
+        for(int i = 0;i < 10;i++)
+        {
+            m_Rigidbody2D.velocity = new Vector2(1 * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+            yield return null;
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            m_Rigidbody2D.velocity = new Vector2(-1 * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+            yield return null;
+        }
+
+        m_isRunning = false;
+    }
+
+    public void Jump()
+    {
+        // If the player should jump...
+        if ((bool)Physics2D.Linecast(this.transform.position, new Vector2(this.transform.position.x, this.transform.position.y - 0.539f), m_WhatIsGround))
+        {
+            // Add a vertical force to the player.
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
         }
     }
 }
